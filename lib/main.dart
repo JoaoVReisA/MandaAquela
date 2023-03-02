b83bdb4f8ef9d1ee3ee671809707a6518b234f7a
João Victor Reis Alves <joao.reisa13@gmail.com>
2023-03-02 20:06:45 -0300

    Configure firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:manda_aquela/app.dart';
 
 import 'core/modules/app_module.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(ModularApp(
-    module: AppModule(),
-    child: const AppWidget(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
 }
