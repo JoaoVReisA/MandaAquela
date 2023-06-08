66e50561f2846db20c8762991ab92d7cdcf0e1df
João Victor Reis Alves - JAV <joao.reisa13@gmail.com>
2023-06-08 19:00:34 -0300

    start home page

--- lib/presenter/home/home_page.dart
+++ lib/presenter/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:manda_aquela/domain/entities/user_firebase_info.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +11,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+        title: Text('Olá,\n${UserFirebaseInfo.instance.name}'),
+        elevation: 0,
+        actions: [Container()],
+      ),
+      body: const Center(
         child: Text('Home'),
       ),
     );
